7f738cc7d2ee3b204daf24f32be1d5f197a6e201
viimo-dev <utils@viimo.app>
2025-01-12 18:20:10 +0000

    Añadido create event

--- src/usecases/EventManager.js
+++ src/usecases/EventManager.js
@@ -13,7 +13,29 @@ export class EventManager {
       container.innerHTML = "";
 
       events.forEach((event) => {
-        // Tu lógica para pintar tarjetas de eventos, etc.
+        const eventCard = new EventCardComponent(event);
+
+
+        // Opción para eliminar evento
+        eventCard.setDeleteAction(async (eventId) => {
+          try {
+            console.log(`Intentando eliminar evento con ID ${eventId}`);
+            await EventAPIAdapter.deleteEvent(eventId);
+            console.log(`Evento con ID ${eventId} eliminado correctamente.`);
+            await this.renderEventsInGrid(containerId); // Recargar el grid
+          } catch (error) {
+            if (error.status === 404) {
+              console.warn(`El evento con ID ${eventId} no se pudo eliminar del backend`);
+              eventCard.setErrorText("No se puede eliminar del backend. API devuelve 404.");
+            } else {
+              console.error(`Error al eliminar evento con ID ${eventId}:, error.message`);
+              eventCard.setErrorText(`Error: ${error.message}`);
+            }
+          }
+        });
+
+
+        container.appendChild(eventCard.getElement());
       });
     } catch (error) {
       console.error("Error al renderizar eventos:", error);
