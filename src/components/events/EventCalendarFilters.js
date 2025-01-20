81783ee5a12ff2b88d33f350afeee08ca13ae0a8
viimo-dev <utils@viimo.app>
2025-01-20 11:33:17 +0000

    Añadiendo el calendario de eventos

--- src/components/events/EventCalendarFilters.js
+++ src/components/events/EventCalendarFilters.js
@@ -12,37 +12,41 @@ export class EventCalendarFilters {
     createElement() {
       const template = document.createElement("template");
       template.innerHTML = `
-        <form id="events-filters-container" class="events-header-filters">
-          <div class="filter-group">
-            <label>
-              <input type="radio" name="event-type" value="all" checked>
-              All events
+        <div class="form w-form">
+          <form id="events-filters" name="events-filters" class="events-header-filters">
+            <label id="all-events" class="filtro w-radio">
+              <div class="w-form-formradioinput w-form-formradioinput--inputType-custom radio-buton w-radio-input w--redirected-checked"></div>
+              <input type="radio" name="event-type" value="all" style="opacity:0;position:absolute;z-index:-1" checked>
+              <span class="paragraph w-form-label">All events</span>
             </label>
-            <label>
-              <input type="radio" name="event-type" value="upcoming">
-              Only upcoming
+            <label id="upcoming-events" class="filtro w-radio">
+              <div class="w-form-formradioinput w-form-formradioinput--inputType-custom radio-buton w-radio-input"></div>
+              <input type="radio" name="event-type" value="upcoming" style="opacity:0;position:absolute;z-index:-1">
+              <span class="paragraph w-form-label">Only upcoming</span>
             </label>
-          </div>
-          <div class="filter-group">
-            <label>
-              <input type="radio" name="time-frame" value="month" checked>
-              Month
+            <div class="filters-separator"></div>
+            <label id="month-events" class="filtro w-radio">
+              <div class="w-form-formradioinput w-form-formradioinput--inputType-custom radio-buton w-radio-input w--redirected-checked"></div>
+              <input type="radio" name="time-frame" value="month" style="opacity:0;position:absolute;z-index:-1" checked>
+              <span class="paragraph w-form-label">Month</span>
             </label>
-            <label>
-              <input type="radio" name="time-frame" value="year">
-              Year
+            <label id="events-year" class="filtro w-radio">
+              <div class="w-form-formradioinput w-form-formradioinput--inputType-custom radio-buton w-radio-input"></div>
+              <input type="radio" name="time-frame" value="year" style="opacity:0;position:absolute;z-index:-1">
+              <span class="paragraph w-form-label">Year</span>
             </label>
-          </div>
-          <div class="filters-separator"></div>
-          <div class="events-name-filter">
-            <input 
-              type="text" 
-              placeholder="Filter by name" 
-              id="filter-by-name" 
-              class="name-filter-input"
-            />
-          </div>
-        </form>
+            <div class="filters-separator"></div>
+            <div class="events-name-filter">
+              <img src="https://cdn.prod.website-files.com/677e577ef5b94953b8c1831e/677edba3ffe29468b361c65c_Vector.png" alt="">
+              <input 
+                type="text" 
+                placeholder="Filter by name" 
+                id="filter-by-name" 
+                class="events-name-filter-field w-input"
+              />
+            </div>
+          </form>
+        </div>
       `.trim();
   
       const element = template.content.firstChild;
