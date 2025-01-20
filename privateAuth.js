6eec7ddb38dfab70f5b287aa90fc52c4bd877e31
viimo-dev <utils@viimo.app>
2025-01-20 08:38:59 +0000

    Trabajando datos de checkfront

---
+++ privateAuth.js
@@ -0,0 +1,6 @@
+export class PrivateAuth {
+    static username = '48efbe3d37b39c90e20f2ab2f9817450c8102f4f';
+    static password = '5ae808991d2427d7e3d8ee5d669c1078f76030c292a342b4b8fd9b94f1c2b8b7';
+  }
+  
+
